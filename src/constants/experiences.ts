7f6a161b30469838e15b424ba611bd954906daa5
Rohan Gope <rg2512@it.jgec.ac.in>
2024-08-23 23:39:43 +0530

    chore: Update experiences and add new internship details

--- src/constants/experiences.ts
+++ src/constants/experiences.ts
@@ -1,31 +1,36 @@
 export const myExperiences = [
-    {
-        company: "Celebel Technologies",
-        title: "React Developer Intern",
-        date: "Apr 2021 - Present",
-        description: [
-            "Developed a web application for a client using ReactJS, Redux and Firebase.",
-            "Learning and implementing new technologies and tools.",
-        ]
-    },
-    {
-        company: "Nova Web Solutions",
-        title: "Software Developer Intern",
-        date: "Jan 2024 - Mar 2024",
-        description: [
-            "Worked on backend projects using Node.js, Express.js and Mysql.",
-            "Researched translation APIs and suggested the company use the DeepL translation API.",
-            "Utilized Jest for testing."
-        ]
-    },
-    {
-        company: "INFOMATICAE TECHNOLOGY PVT. LTD",
-        title: "Software Developer Intern",
-        date: "Jun 2023 - Aug 2023 ",
-        description: [
-            "Worked on Augmented Reality Project.",
-            "Utilized Unity for the development."
-        ]
-    }
-
+	{
+		company: 'ASR Tech Solutions',
+		title: 'Web Development and Mobile App Development Intern',
+		date: 'Apr 2021 - Present',
+		description: [
+			'Developed a Mobile Application for clients using React Native',
+			'Built a web application for clients using MERN.'
+		]
+	},
+	{
+		company: 'Celebel Technologies',
+		title: 'React Developer Intern',
+		date: 'Apr 2024 - Jun 2024',
+		description: [
+			'Developed a web application for a client using ReactJS, Redux and Firebase.',
+			'Learning and implementing new technologies and tools.'
+		]
+	},
+	{
+		company: 'Nova Web Solutions',
+		title: 'Software Developer Intern',
+		date: 'Jan 2024 - Mar 2024',
+		description: [
+			'Worked on backend projects using Node.js, Express.js and Mysql.',
+			'Researched translation APIs and suggested the company use the DeepL translation API.',
+			'Utilized Jest for testing.'
+		]
+	},
+	{
+		company: 'INFOMATICAE TECHNOLOGY PVT. LTD',
+		title: 'Software Developer Intern',
+		date: 'Jun 2023 - Aug 2023 ',
+		description: ['Worked on Augmented Reality Project.', 'Utilized Unity for the development.']
+	}
 ]
